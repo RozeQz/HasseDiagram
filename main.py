fb80420c7746327408f20a8f174da4c1156e5e2d
Lisa Rozhkova <liza.roz@yandex.ru>
2022-11-04 17:46:39 +0300

    Add new windows

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout
 from ui_mainwindow import Ui_MainWindow
+from ui_mainmenu import Ui_MainMenu
 
 def ui_application():
+    # app = QApplication(sys.argv)
+    # window = QMainWindow()
+    # ui = Ui_MainWindow()
+    # ui.setupUi(window)
+    # window.show()
+    # sys.exit(app.exec())
+
     app = QApplication(sys.argv)
     window = QMainWindow()
-    ui = Ui_MainWindow()
+    ui = Ui_MainMenu()
     ui.setupUi(window)
     window.show()
     sys.exit(app.exec())
